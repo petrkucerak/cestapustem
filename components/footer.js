ef7d5daa95edb2b826660a6437ef6982708092b7
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-12-09 18:25:55 +0100

    Change text on the footer.

--- components/footer.js
+++ components/footer.js
@@ -6,35 +6,18 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            STC Zpěvníček
+            Cesta půstem
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <span>
-              <span>
-                {" "}
-                <strong>Disclaimer:</strong> Všechny texty písniček jsou dílem
-                příslušných autorů. Nepřisvojujeme si k nim vlastnictví ani
-                žádná jiná práva.
-              </span>
-              <br />
-              <br />
-              Vytvořeno Péťou a&nbsp;
+              ...
               <a
                 className="hover:underline"
                 target="_blank"
                 rel="noreferrer noopener"
-                href="https://trnck.dev/"
+                href="https://github.com/petrkucerak/cestapustem/"
               >
-                Fílou
-              </a>
-              &nbsp;s láskou na&nbsp;
-              <a
-                className="hover:underline"
-                target="_blank"
-                rel="noreferrer noopener"
-                href="https://github.com/microsoftstc/zpevnicek"
-              >
-                GitHubu
+                repo na GitHub
               </a>
             </span>
             <br />
