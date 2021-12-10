553fa4fa18f64f9e08992dfe9503d5022bc25f6c
Petr Kučera <petr@khome.cz>
2021-12-10 16:20:41 +0000

    Fix header hover effect.

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <h2 className="text-3xl font-bold mx-8 mt-4 underline hover:text-grey ease-in-out transition duration-150">
+    <h2 className="mt-4 mx-8">
       <Link href="/">
-        <a className="">Cesta půstem</a>
+        <a className="text-3xl font-bold underline hover:text-grey ease-in-out transition duration-150">
+          Cesta půstem
+        </a>
       </Link>
     </h2>
-  )
+  );
 }
