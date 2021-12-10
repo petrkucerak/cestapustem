159764f108457d4e5bcb13339dd7d4ab88430b59
Petr Kučera <petr@khome.cz>
2021-12-10 10:56:56 +0000

    Custom color & font-size

--- styles/README.md
+++ styles/README.md
@@ -2,6 +2,6 @@
 
 Dark: #363636ff;
 Grey: #4d4d4dff;
-Grey-darker: #404040ff;
+Grey-dark: #404040ff;
 Yellow: #f9d43fff;
 Yellow-darker: #f2cd41ff;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 module.exports = {
-  darkMode: 'media',
-  mode: 'jit',
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  darkMode: "media",
+  mode: "jit",
+  purge: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        'dark': '#363636ff',
+        'grey': '#4d4d4dff',
+        'grey-dark': '#404040ff',
+        'yellow': '#f9d43fff',
+        'yellow-dark': '#f2cd41ff',
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
     },
   },
-}
+};
