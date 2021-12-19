14a5aba3bd1b8005b4090db8f051aedbbb2c2fed
Petr Kučera <petr@khome.cz>
2021-12-19 21:42:06 +0000

    Config cache & add page with offline info.

---
+++ pages/_offline.js
@@ -0,0 +1,24 @@
+import Container from "../components/container";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import Head from "next/head";
+
+export default function Offline() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Cesta půstem</title>
+        </Head>
+        <Container>
+          <Intro />
+          <div className="text-2xl">
+            Vypadá to, že nejsi připojen k internetu, (a možná) se ti texty
+            tohoto dne ještě nezapsaly do paměti zařízení. Zkus to prosím znovu,
+            později.
+          </div>
+        </Container>
+      </Layout>
+    </>
+  );
+}
