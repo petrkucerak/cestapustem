77886eabe9125414ce2ec82a3814dd0248b93643
Petr Kučera <petr@khome.cz>
2021-12-19 21:36:49 +0000

    Better links

--- components/more-information.js
+++ components/more-information.js
@@ -5,17 +5,17 @@ export default function MoreInfromation() {
     <section className="px-8 mb-5">
       <h2 className="text-2xl font-bold mt-4 mb-2">Více informací</h2>
       <div className="flex flex-wrap justify-between">
-        <Link href="/how-read">
+        <Link href="/how-read/">
           <a className="uppercase bg-grey-light duration-200 hover:bg-grey-dark	text-white font-bold py-2 px-4 my-2 rounded">
             Jak číst brožuru
           </a>
         </Link>
-        <Link href="/authors">
+        <Link href="/authors/">
           <a className="uppercase bg-grey-light duration-200 hover:bg-grey-dark	text-white font-bold py-2 px-4 my-2 rounded">
             autoři textů
           </a>
         </Link>
-        <Link href="/install">
+        <Link href="/install/">
           <a className="uppercase bg-grey-light duration-200 hover:bg-grey-dark	text-white font-bold py-2 px-4 my-2 rounded">
             instalace na mobil
           </a>
