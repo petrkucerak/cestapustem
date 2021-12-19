b1c67a4db736c3f1f691c6397ba1223c5c43f0b8
Petr Kučera <petr@khome.cz>
2021-12-19 13:00:53 +0000

    Add no next.js redirect

--- components/day-calendar.js
+++ components/day-calendar.js
@@ -3,69 +3,74 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function DayCalendar({ posts }) {
-  const [usedCaps, setUsedCaps] = useState([]);
   const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+    "neděle",
+    "pondělí",
+    "úterý",
+    "středa",
+    "čtvrtek",
+    "pátek",
+    "sobota",
   ];
   const months = [
-    "Leden",
-    "Únor",
-    "Březen",
-    "Duben",
-    "Květen",
-    "Červen",
-    "Červenec",
-    "Srpen",
-    "Září",
-    "Říjen",
-    "Listopad",
-    "Prosinec",
+    "ledna",
+    "února",
+    "březena",
+    "dubena",
+    "květena",
+    "červena",
+    "červenece",
+    "srpena",
+    "září",
+    "října",
+    "listopadu",
+    "prosinece",
+  ];
+  const monthsConst = [
+    "leden",
+    "únor",
+    "březen",
+    "duben",
+    "květen",
+    "červen",
+    "červenec",
+    "srpen",
+    "září",
+    "říjen",
+    "listopad",
+    "prosinec",
   ];
   return (
     <section>
       <h2 className="text-2xl font-bold mx-8 mt-4 mb-2">Konkrétní den</h2>
-      <div className="grid grid-cols-7 gap-4 px-8">
-        {posts.map((post) => {
-          let date_t = new Date(post.date);
+      <div className="px-8">
+        <select
+          name="day"
+          onChange={(e) => {
+            window.location.href = "/day/" + e.target.value;
+            console.log(e.target.value);
+          }}
+          className="text-black w-3/5"
+        >
+          {posts.map((post) => {
+            let date_t = new Date(post.date);
 
-          let doCapital = true;
-          if (posts.indexOf(post) > 0) {
-            let monthFirst = new Date(posts[posts.indexOf(post) - 1].date);
-            doCapital = !(monthFirst.getMonth() === date_t.getMonth());
-            console.log("Mont is: ", date_t.getMonth());
-          }
-
-          return (
-            // <div>
-            //   {doCapital && (
-            //     <li className="col-start-1 col-end-8">
-            //       {doCapital && <span>{months[date_t.getMonth()]}</span>}
-            //     </li>
-            //   )}
-            //   <li className="">
-            <PostPreview
-              key={post.date}
-              slug={post.slug}
-              day={post.day}
-              date={post.date}
-              quote={post.quote}
-              reflexion={post.reflexion}
-              author={post.author}
-              description={post.description}
-              preayer={post.preayer}
-              link={post.link}
-            />
-            
-            //   </li>
-            // </div>
-          );
-        })}
+            let doCapital = true;
+            if (posts.indexOf(post) > 0) {
+              let monthFirst = new Date(posts[posts.indexOf(post) - 1].date);
+              doCapital = !(monthFirst.getMonth() === date_t.getMonth());
+            }
+            return (
+              <option value={post.slug}>
+                {days[date_t.getDay()] +
+                  " " +
+                  date_t.getDate().toString() +
+                  ". " +
+                  months[date_t.getMonth()]}
+              </option>
+            );
+          })}
+        </select>
       </div>
     </section>
   );
