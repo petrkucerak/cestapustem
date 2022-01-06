b7a306d23e161b455f27a5b07e311d5442b779c9
Petr Kučera <petr@khome.cz>
2022-01-06 17:10:27 +0000

    Add setings comment.

--- next.config.js
+++ next.config.js
@@ -23,8 +23,7 @@ module.exports = withPWA({
   },
   pwa: {
     dest: "public",
-    cacheOnFrontEndNav: true,
+    cacheOnFrontEndNav: true, // make all page linked form index cached
     disable: process.env.NODE_ENV === "development",
-    // sw: "/pwabuilder-sw.js",
   },
 });
