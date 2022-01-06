c3c1a06de625724081d5e7803f2990e8fecc0f31
Petr Kučera <petr@khome.cz>
2022-01-06 21:53:26 +0000

    Create separe component for each month.

--- components/day-calendar.js
+++ components/day-calendar.js
@@ -1,65 +1,14 @@
 import Link from "next/dist/client/link";
+import MonthDates from "../components/month-dates";
 
 export default function DayCalendar({ posts }) {
 
-  const days = [
-    "neděle",
-    "pondělí",
-    "úterý",
-    "středa",
-    "čtvrtek",
-    "pátek",
-    "sobota",
-  ];
-  const months = [
-    "leden",
-    "únor",
-    "březen",
-    "duben",
-    "květen",
-    "červen",
-    "červenec",
-    "srpen",
-    "září",
-    "říjen",
-    "listopad",
-    "prosinec",
-  ];
-
-  function generateSlugFromDate(date) {
-    let month = date.getMonth() + 1;
-    return `/day/${date.getFullYear()}-${month
-      .toString()
-      .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}/`;
-  }
 
   return (
     <section>
       <h2 className="text-2xl font-bold mx-8 mt-4 mb-2">Přejít na konkrétní den</h2>
-      <div className="mx-8 flex flex-wrap justify-start flex-row">
-        {posts.map((post) => {
-          let date_t = new Date(post.date);
-          let str = `${date_t.getDate() + 1}.`;
-          let doCapital = true;
-          if (posts.indexOf(post) > 0) {
-            let dateA = new Date(post.date);
-            let dateF = new Date(posts[posts.indexOf(post) - 1].date);
-            doCapital = !(
-              dateA.getMonth() === dateF.getMonth()
-            );
-          }
-          return (
-            <div key={post.slug} className="mr-4 mb-4">
-              {doCapital && <span className="">{months[date_t.getMonth()]}</span>}
-              <Link href={generateSlugFromDate(date_t)}>
-                <a className="uppercase text-white duration-200 hover:bg-grey-dark font-bold py-2 px-4 rounded">
-                  {str}
-                </a>
-              </Link>
-            </div>
-          )
-        })}
-      </div>
+      <MonthDates posts={posts} month={0} />
     </section>
-  );
+  )
+
 }
---
+++ components/month-dates.js
@@ -0,0 +1,57 @@
+import Link from "next/dist/client/link";
+
+export default function MonthDates({ month, posts }) {
+  const days = [
+    "neděle",
+    "pondělí",
+    "úterý",
+    "středa",
+    "čtvrtek",
+    "pátek",
+    "sobota",
+  ];
+  const months = [
+    "leden",
+    "únor",
+    "březen",
+    "duben",
+    "květen",
+    "červen",
+    "červenec",
+    "srpen",
+    "září",
+    "říjen",
+    "listopad",
+    "prosinec",
+  ];
+
+  function generateSlugFromDate(date) {
+    let month = date.getMonth() + 1;
+    return `/day/${date.getFullYear()}-${month
+      .toString()
+      .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}/`;
+  }
+
+  const firstDate = new Date(posts[0].date);
+
+  return (
+
+    <div className="mx-8">
+      <h3>{months[month]}</h3>
+      <div className="flex flex-wrap justify-start flex-row">
+        {posts.map((post) => {
+          let date_t = new Date(post.date);
+          let str = `${date_t.getDate() + 1}.`;
+          return (
+            <Link key={post.slug} href={generateSlugFromDate(date_t)}>
+              <a className="mr-4 mb-4 uppercase text-white duration-200 hover:bg-grey-dark font-bold py-2 px-4 rounded">
+                {str}
+              </a>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+
+  );
+}
