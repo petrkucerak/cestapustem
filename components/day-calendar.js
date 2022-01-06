8ed12e2cb782edd710090f1848e4d7a7c8eee95b
Petr Kučera <petr@khome.cz>
2022-01-06 19:47:36 +0000

    Import react calendar.

--- components/day-calendar.js
+++ components/day-calendar.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Calendar from "react-calendar";
 
 export default function DayCalendar({ posts }) {
   const router = useRouter();
@@ -27,28 +28,14 @@ export default function DayCalendar({ posts }) {
     "prosinece",
   ];
 
+  let firstDate = new Date(posts[0].date);
+  const emptyDays = [...Array(firstDate.getDate())];
+  let i = 0;
+
   return (
     <section>
       <h2 className="text-2xl font-bold mx-8 mt-4 mb-2">Přejít na konkrétní den</h2>
-      <table className="mx-8">
-        <tr>
-          <th>PO</th>
-          <th>UT</th>
-          <th>ST</th>
-          <th>CT</th>
-          <th>PA</th>
-          <th>SO</th>
-          <th>NE</th>
-        </tr>
-        <tr>
-          {posts.map((post) => {
-            let date_t = new Date(post.date);
-            return (
-              <td key={post.slug}>{date_t.getDate()}</td>
-            )
-          })}
-        </tr>
-      </table>
+      <Calendar/>
     </section>
   );
 }
