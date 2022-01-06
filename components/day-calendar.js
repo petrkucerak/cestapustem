b0e4b001c8561f292bca4d953290ba4a6b3c8c1b
Petr Kučera <petr@khome.cz>
2022-01-06 23:40:23 +0100

    Better calendar. (#18)
    * ...
    
    * !...
    
    * Create header of the table structure.
    
    * Import react calendar.
    
    * Create boxes.
    
    * Insert month names.
    
    * Remove unused code.
    
    * Create separe component for each month.
    
    * Create components for print months separate.
    
    * The simply style of dates.

--- components/day-calendar.js
+++ components/day-calendar.js
@@ -1,58 +1,35 @@
-import { useRouter } from "next/router";
+import MonthDates from "../components/month-dates";
 
 export default function DayCalendar({ posts }) {
-  const router = useRouter();
 
-  const days = [
-    "neděle",
-    "pondělí",
-    "úterý",
-    "středa",
-    "čtvrtek",
-    "pátek",
-    "sobota",
-  ];
-  const months = [
-    "ledna",
-    "února",
-    "březena",
-    "dubena",
-    "květena",
-    "červena",
-    "červenece",
-    "srpena",
-    "září",
-    "října",
-    "listopadu",
-    "prosinece",
-  ];
+  let month_tmp = 0;
+  let data = new Array;
+  data.push(new Array);
+  let ind = 0;
+
+  for (let i = 0; i < posts.length; i++) {
+    let date_tmp = new Date(posts[i].date);
+    // create a new node
+    if (month_tmp != date_tmp.getMonth()) {
+      month_tmp = date_tmp.getMonth();
+      data.push(new Array);
+      ind++;
+    }
+    data[ind].push(posts[i]);
+  }
 
   return (
     <section>
       <h2 className="text-2xl font-bold mx-8 mt-4 mb-2">Přejít na konkrétní den</h2>
-      <div className="px-8">
-        <select
-          name="day"
-          onChange={(e) => {
-            router.push("/day/" + e.target.value + "/");
-          }}
-          className="text-black w-full border-2 border-black rounded h-10"
-        >
-          <option value="/" className="text-grey-light">
-            vyber den
-          </option>
-          {posts.map((post) => {
-            let date_t = new Date(post.date);
-            return (
-              <option key={post.slug} value={post.slug}>
-                {days[date_t.getDay()]} {date_t.getDate().toString()}
-                {". "}
-                {months[date_t.getMonth()]}
-              </option>
-            );
-          })}
-        </select>
-      </div>
+      {data.map((part) => {
+        let date_m = new Date(part[0].date);
+        let mon = date_m.getMonth();
+        return (
+          <div key={date_m}>
+            <MonthDates posts={part} month={mon} />
+          </div>
+        )
+      })}
     </section>
-  );
+  )
 }
---
+++ components/month-dates.js
@@ -0,0 +1,55 @@
+import Link from "next/dist/client/link";
+
+export default function MonthDates({ month, posts }) {
+  const days = [
+    "neděle",
+    "pondělí",
+    "úterý",
+    "středa",
+    "čtvrtek",
+    "pátek",
+    "sobota",
+  ];
+  const months = [
+    "leden",
+    "únor",
+    "březen",
+    "duben",
+    "květen",
+    "červen",
+    "červenec",
+    "srpen",
+    "září",
+    "říjen",
+    "listopad",
+    "prosinec",
+  ];
+
+  function generateSlugFromDate(date) {
+    let month = date.getMonth() + 1;
+    return `/day/${date.getFullYear()}-${month
+      .toString()
+      .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}/`;
+  }
+
+  return (
+
+    <div className="mx-8">
+      <h3 className="text-base uppercase font-bold mt-4 mb-2">{months[month]}</h3>
+      <div className="flex flex-wrap justify-start flex-row">
+        {posts.map((post) => {
+          let date_t = new Date(post.date);
+          let str = `${date_t.getDate() + 1}.`;
+          return (
+            <Link key={post.slug} href={generateSlugFromDate(date_t)}>
+              <a className="mr-4 mb-4 uppercase text-white duration-200 hover:bg-grey-dark font-bold py-2 px-4 rounded">
+                {str}
+              </a>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+
+  );
+}
