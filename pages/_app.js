ceac87f985a60fd8a1e842bbc9bc827148b33b6b
Petr Kučera <petr@khome.cz>
2022-01-14 19:25:09 +0100

    Add integration with One Signal. (#30)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import '../styles/index.css'
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "c0faad38-a3b1-4bd5-8faf-c7f6b109f32d",
+        notifyButton: {
+          enable: true,
+        },
+
+        allowLocalhostAsSecureOrigin: true,
+      });
+    });
+
+    return () => {
+      window.OneSignal = undefined;
+    };
+  }, []); // <-- run this effect once on mount
   return <Component {...pageProps} />
 }
