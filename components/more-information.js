06a6bef2e8e9a78535ebfae40fc9f4f2bd293d41
Petr Kučera <petr@khome.cz>
2022-02-20 20:28:12 +0000

    Add new buttons

--- components/more-information.js
+++ components/more-information.js
@@ -11,13 +11,23 @@ export default function MoreInfromation() {
           </a>
         </Link>
         <Link href="/authors/">
-          <a className="uppercase bg-yellow duration-200 hover:bg-grey-dark	text-white font-bold py-2 px-4 my-2 mr-4 rounded">
+          <a className="uppercase bg-yellow duration-200 hover:bg-grey-dark hover:text-white text-grey font-bold py-2 px-4 my-2 mr-4 rounded">
             autoři textů
           </a>
         </Link>
         <Link href="/install/">
           <a className="uppercase bg-blue duration-200 hover:bg-grey-dark	text-white font-bold py-2 px-4 my-2 mr-4 rounded">
             instalace na mobil
+          </a>
+        </Link>
+        <Link href="/introduction#Jan">
+          <a className="uppercase bg-orange duration-200 hover:bg-grey-dark	text-white font-bold py-2 px-4 my-2 mr-4 rounded">
+            slovo biskupa Jana
+          </a>
+        </Link>
+        <Link href="/introduction#Prokop">
+          <a className="uppercase bg-yellow duration-200 hover:bg-grey-dark	hover:text-white text-grey font-bold py-2 px-4 my-2 mr-4 rounded">
+            slovo o. Prokopa Brože
           </a>
         </Link>
       </div>
