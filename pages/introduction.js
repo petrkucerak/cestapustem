e1868f9f71dd3a0edf18da1d3b114a425a52d3ed
Petr Kučera <petr@khome.cz>
2022-02-20 20:24:13 +0000

    Add instroductions

---
+++ pages/introduction.js
@@ -0,0 +1,121 @@
+import Layout from "../components/layout";
+import Head from "next/head";
+import Header from "../components/header";
+import Container from "../components/container";
+import PostTitle from "../components/post-title";
+
+export default function HowRead() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Předmluva biskupa Jana a Prokopa Brože - Cesta půstem</title>
+        </Head>
+        <Container>
+          <Header></Header>
+          <PostTitle>Předmluva biskupa Jana a Prokopa Brože</PostTitle>
+          <section className="max-w-screen-lg mx-8 text-lg leading-7 mb-4">
+            <h2 id="Jan" className="text-2xl font-bold mt-7">
+              Slovo biskupa Jana Vokála
+            </h2>
+            <p className="my-2">
+              Bratři a sestry v Kristu, milí čtenáři, půst a energie mládí jako
+              by zdánlivě nešly dohromady. Brožura, kterou držíte v rukou,
+              potvrzuje opak. Křesťanský půst totiž není obdobím strnulosti a
+              pasivity, ale naopak aktivního ponoru do hloubky. Síla, odvaha a
+              dravost mládí je proto tou správnou cestou k naplnění jeho smyslu.
+              Ostatně i Ježíš učedníkům říká, že cílem obrácení je přistupovat
+              ke světu, k druhým i k Bohu jako mladý (dokonce{" "}
+              <em>„jako dítě“</em> – srov. Mt 18,3), tedy s činorodou energií,
+              odvahou a důvěrou. S ryzím nadšením, s neunavenou vírou.
+            </p>
+            <p className="my-2">
+              Texty k letošním postním meditacím proto napsali právě mladí lidé
+              do 26 let, jejich průvodci a kaplani. Inspirováni heslem letošního
+              Celostátního setkání mládeže{" "}
+              <em>„Vstaň. Udělám z tebe svědka toho, co jsi viděl.“</em> (Sk
+              26,16), svědčí o své cestě za Kristem a s Kristem. Není to však
+              jen čtení od mladých pro mladé. Jsou to texty pro nás všechny,
+              abychom své postní období obohatili právě „mladým přístupem“.
+              Abychom se od mladých autorů mohli učit tomu, co jsme možná už
+              zapomněli, ale k čemu nás Kristus stále vyzývá: vstát a následovat
+              Ho se vší rozhodností, elánem a zápalem.
+            </p>
+            <p className="my-2">
+              K tomu Vám na prahu postní doby ze srdce žehnám!
+            </p>
+            <span className="my-2">Váš biskup +Jan</span>
+
+            <h2 id="Prokop" className="text-2xl font-bold mt-7">
+              Slovo otce Prokopa Brože
+            </h2>
+            <p className="my-2">
+              V brožuře jsou uvedeny úryvky z posynodáln apoštolské exhortace
+              Christus vivit – Kristus žije, kterou vydal papež František na
+              základě biskupské synody, která se konala v roce 2018 na téma
+              <em> Mladí, víra a rozlišování povolání.</em> Vážení přátelé,
+              bratři a sestry, v posledních letech stále více chápu dobu postní
+              jako čas, který nám je dán, aby hojil a léčil. Papež František v
+              textu Dopisy o utrpení popisuje situaci Tovaryšstva Ježíšova v
+              době, kdy byl řád bulou Klementa XIV.<em>Dominus ac Redemptor</em>{" "}
+              v roce 1773 zrušen a pak bulou Pia VII.{" "}
+              <em>Sollicitudo omnium ecclesiarum</em> v roce 1814 obnoven.
+            </p>
+            <p className="my-2">
+              Říkalo se a šuškalo kdeco, bratři byli <em>zmateni.</em> Diskuse a
+              myšlenkové rozbory bratřím pokoj a sílu nepřinášely. Naopak, jen
+              umocňovaly jejich nepokoj a beznaděj. Následné pronásledování v
+              nich vyvolalo hněv, vnitřní splašenost, postoj rezignace,
+              zarputilost, stravovali se sami v sobě. Generální představení
+              reagovali tím, že otce a bratry těšili, znovu v nich posilovali
+              vědomí sepětí s Tovaryšstvem a učili je, dle papeže Františka,{" "}
+              <em>nauce o utrpení.</em> Je nutné se ponořit do modlitby a
+              prosit: „Proste, a bude vám dáno“ (Mt 7,7). Ježíš právě o takové
+              chvíli říká: „Jestliže tedy vy, třebaže jste zlí, umíte dávat svým
+              dětem dobré dary, čím spíše nebeský Otec dá Ducha Svatého těm, kdo
+              ho prosí!“ (Lk 11,13). V modlitbě, jak píše v roce 1759 generální
+              představený Lorenzo Ricci, je třeba hledat <em>dobrého ducha,</em>{" "}
+              který nás vede k pokání, k pokoře a správnému poznání sebe i toho,
+              co nás obklopuje. Odtud je možné <em>poznat</em> vlastní selhání i
+              hříchy. A současně je třeba prosit o{" "}
+              <em>ducha milosti a modlitby,</em> o ducha, kterého Bůh vlil do
+              společného díla Tovaryšstva už na počátku a stále ho udržuje.
+            </p>
+            <p className="my-2">
+              Učíme se v těchto týdnech a měsících díky synodálnímu procesu{" "}
+              <em>nově naslouchat.</em> Novum může spočívat v tom, že zbystříme
+              svou mysl i srdce natolik, že lépe budeme naslouchat svým bližním.
+              Zatímco jim nasloucháme, zaznamenáváme ve svém srdci hlas Ducha
+              Svatého, probouzí se naše paměť a oživuje se naše schopnost
+              doufat. Jsme schopni vnímat nové souvislosti a v nich nově pojímat
+              a rozlišovat situace, které po nás žádají rozhodnutí. A možná
+              budeme schopni takto očištěnou a osvícenou novost ducha sdílet i
+              mezi sebou navzájem. Postní brožura, kterou otevíráte, k tomu chce
+              dopomoci.
+            </p>
+            <p className="my-2">
+              Letos se připravujeme v Hradci Králové na Celostátní setkání
+              mládeže, které proběhne ve dnech 10.–14. 8. 2022. V souvislosti s
+              probíhající diecézní fází biskupské synody o synodalitě se často
+              uvádí, že svatý Benedikt ve své řeholi nabádá naslouchat těm
+              nejmladším mnichům, protože „Bůh často zjevuje to, co je nejlepší,
+              mladšímu.“ A proto jsme se rozhodli, že svěříme přípravu textů
+              postní brožury mladým. Jde o mladé napříč celou diecézí, k nim
+              jsme oslovili také několik kaplanů pro mládež, kteří působí v
+              našich vikariátech.
+            </p>
+            <p className="my-2">
+              Vznik letošní postní brožury proto provázela krásná spolupráce se
+              47 mladými lidmi naší diecéze a ostatními, kteří pomohli jejich
+              texty připravit pro tisk. Kéž společná meditace evangelia, pozorná
+              četba zamyšlení mladých a modlitba pomohou nám všem dobře se
+              připravit na slavení radostné oslavy Kristova vzkříšení!
+            </p>
+
+            <span className="my-2">Prokop Brož</span>
+          </section>
+        </Container>
+      </Layout>
+    </>
+  );
+}
