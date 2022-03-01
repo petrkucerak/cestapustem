1f07a2a2627939efcf3ead7786680d7aba4ac50a
Petr Kučera <petr@khome.cz>
2022-03-01 12:26:44 +0000

    Set up long description.

--- components/intro.js
+++ components/intro.js
@@ -28,7 +28,7 @@ export default function Intro() {
         <Image src="/icon.svg" height={200} width={200} alt="App icon"></Image>
       </div>
       <h1 className="text-4xl font-bold mx-8 mb-1">Cesta půstem</h1>
-      <p className="text-lg mx-8">Duchovní průvodce postní dobou z&nbsp;dílny Královéhradecké diecéze.</p>
+      <p className="text-lg mx-8">Cesta půstem je webová aplikace k tištěné verzi knihy Půst, která se každoročně vydává v Královéhradecké diecézi. Letos do knihy přispívali hlavně mladí z naší diecéze. Každý den si můžeš přečíst evangelium, zamyšlení, krátký úryvek z exhortace papeže Františka Christus vivit a krátkou modlitbu.</p>
       <h2 className="text-2xl font-bold mx-8 mt-4 mb-2">Číst text</h2>
       <div className="mx-8 flex flex-wrap justify-start flex-row">
         <Link href={generateSlugFromDate(yestarday)}>
