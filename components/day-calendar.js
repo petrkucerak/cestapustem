7f84b7bebfed07aea8491023e5bcb0732def8dd4
Petr Kučera <petr@khome.cz>
2022-03-01 12:28:59 +0000

    Set up footer margin button.

--- components/day-calendar.js
+++ components/day-calendar.js
@@ -20,7 +20,7 @@ export default function DayCalendar({ posts }) {
   }
 
   return (
-    <section>
+    <section className="mb-4">
       <h2 className="text-2xl font-bold mx-8 mt-4 mb-2">Přejít na konkrétní den</h2>
       {data.map((part) => {
         let date_m = new Date(part[0].date);
