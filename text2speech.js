2d7391d6cc9cf1e6be35b57896ae9b0cadb06894
Petr Kučera <petr@khome.cz>
2022-03-02 23:42:54 +0000

    Add text

--- text2speech.js
+++ text2speech.js
@@ -1,8 +1,29 @@
 const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const fs = require("fs");
 
 module.exports.text2speech = () => {
     console.log('hi');
-    // Json2Audio();
+    allJsons();
+
+    function allJsons() {
+        fs.readdir("_days/", (err, filenames) => {
+            if (err) {
+                onerror(err);
+                return;
+            }
+            filenames.forEach((filename) => {
+                fs.readFile(`_days/${filename}`, "utf-8", (err, content) => {
+                    if (err) {
+                        onerror(err);
+                        return;
+                    }
+                    let contentJSON = JSON.parse(content);
+                    let text = `Vítej u dnešního zamyšlení na Tvé cestě Půstem!\nDnes je ${contentJSON.day} a autorem zamyšlení je ${contentJSON.author}.\n\nÚryvek z Bible\n${contentJSON.quote}\n\nZamyšlení\n${contentJSON.reflexion}\n\nDnešní sekce z Christus Vivit\n${contentJSON.vivit}\n\nZávěrečná modlitba\n${contentJSON.preayer}`;
+                    console.log(text);
+                });
+            });
+        });
+    }
 
     function SynthesizeSpeech(text, filename, SPEECH_KEY, LOCATION_REGION) {
         const speechConfig = sdk.SpeechConfig.fromSubscription(SPEECH_KEY, LOCATION_REGION);
