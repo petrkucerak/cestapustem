11bcb7551721b9a9cb11ba8bb79e9e7cd3983332
petrkucerak <petr@khome.cz>
2024-02-14 09:34:25 +0100

    Update text

--- pages/index.js
+++ pages/index.js
@@ -26,49 +26,15 @@ export default function Home({ allPosts }) {
         <Calendar posts={allPosts} />
         <h2 className="mx-6 text-2xl uppercase font-bold">O projektu</h2>
         <p className="mx-6 text-lg mb-8">
-          Adventní cesta je webová aplikace k&nbsp;tištěné verzi knihy Advent,
-          která se každoročně vydává v&nbsp;Královéhradecké diecézi. Letos do
-          knihy přispívali katecheté naší diecéze. Každý den si můžeš přečíst
-          evangelium, zamyšlení a&nbsp;krátkou modlitbu.
+          Cesta půstemje webová aplikace k&nbsp;tištěné verzi knihy Půst, která
+          se každoročně vydává v&nbsp;Královéhradecké diecézi. Každý den si
+          můžeš přečíst evangelium, zamyšlení a&nbsp;krátkou modlitbu.
         </p>
         <div className="mx-6 flex flex-row justify-around mb-8 flex-wrap">
           <Button href={"/autori"} content="Autoři" />
-          <Button
-            href={"https://forms.gle/n19QvNFMW5A5RbNBA"}
-            content="Zpětná vazba"
-            target={"_blank"}
-            rel={"noreferrer noopener external"}
-          />
-          <Button
-            href={
-              "https://open.spotify.com/show/2I3GPLeVZdPH8dcuVYCDoJ?si=7ad72e34e408463e"
-            }
-            content="Nahrávky na Spotify"
-            target={"_blank"}
-            rel={"noreferrer noopener external"}
-          />
 
           <Button href={"/slova-biskupa-Jana"} content="Slova biskupa Jana" />
           <Button href={"/slova-Prokopa-Broze"} content="Slova Prokopa Brože" />
-        </div>
-        <div className="mb-8">
-          <h2 className="mx-6 text-2xl uppercase font-bold mb-2">
-            Předchozí ročníky
-          </h2>
-          <ul className="mx-6 list-disc pl-6">
-            <li>
-              <Link
-                href={"https://2022.adventnicesta.cz/"}
-                content="Předchozí adventní brožura"
-                target={"_blank"}
-                rel={"external"}
-                className="underline"
-              >
-                2022
-              </Link>{" "}
-              - autory zamyšlení jsou zaměstnanci charity
-            </li>
-          </ul>
         </div>
       </Layout>
     </>
