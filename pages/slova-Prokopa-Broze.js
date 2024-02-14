2ccef317510c6524eb5fb9da83b25135f89036b2
petrkucerak <petr@khome.cz>
2024-02-14 10:20:19 +0100

    Clean text

--- pages/slova-Prokopa-Broze.js
+++ pages/slova-Prokopa-Broze.js
@@ -6,7 +6,7 @@ export default function SlovaProkopaBrože() {
   return (
     <>
       <Head>
-        <title>Slova Prokopa Brože Adventní cesta</title>
+        <title>Slova Prokopa Brože - Adventní cesta</title>
       </Head>
       <Layout>
         <ArticleHeader />
@@ -16,53 +16,52 @@ export default function SlovaProkopaBrože() {
             Vážení přátelé, bratři a sestry,
           </p>
           <p className="text-lg mb-4">
-            opět se přiblížil krásný čas, při němž se příroda zastavuje,
-            zklidňuje, nastává zimní období a my prožíváme advent. Jak
-            blahodárná a duchovně obnovná doba to může být! Bohužel může
-            nastat i opačná situace, že nám každoročně se vracející advent
-            upadá do rutiny, svou hloubku a skrytou náročnost ztrácí a stane se
-            z něho jen další t(o)var, prožitek, který sotva začal, plyne pryč, a
-            zanechává nás prázdnější, prkennější, vnitřně unavenější.
+            pro letošní putování postní dobou jsme pozváni papežem Františkem,
+            abychom se vydali cestou modlitby, jak uvádí otec biskup Jan. V
+            modlitbě nás Bůh učí, jak hodně se máme životu kolem nás a v nás
+            otevřít. A také nás učí rozšiřovat své nitro, aby bylo schopné
+            pojmout rozlehlost a složitost současného světa (P. Coda). Papež v
+            přípravném dopise k Velkému jubileu 2025 píše, že se na něj máme v
+            předcházejícím roce připravit velkou symfonií modlitby. „Především
+            je třeba obnovit touhu zůstávat v Pánově přítomnosti, naslouchat mu
+            a adorovat ho. V modlitbě pak děkovat Bohu za tolik darů jeho lásky
+            k nám a chválit ho za dílo stvoření, k jehož ochraně a úctě jsme
+            všichni zavázáni. Dále je modlitba hlasem jednoho srdce a jedné duše
+            (srov. Sk 4,32), jež se projevuje solidaritou a sdílením
+            každodenního chleba. Modlitba dovoluje každému muži a každé ženě
+            tohoto světa obrátit se k jedinému Bohu a vyjádřit mu, co se nachází
+            hluboko v jeho srdci. Modlitba je také mistrnou cestou svatosti, jež
+            uvádí do života, kde kontemplace nachází své místo uprostřed
+            činnosti. Tedy půjde o rok intenzívní modlitby, v němž se srdce
+            budou otevírat bohatství milosti, aby modlitba Otče náš, již nás učí
+            sám Ježíš, tvořila životní program každého učedníka.“
           </p>
           <p className="text-lg mb-4">
-            Advent je vzácný čas, ve kterém nám pomáhají posvátné texty Písma
-            svatého nově a znovu objevovat naši budoucnost. Kam směřujeme? Čím
-            musíme projít, abychom dosáhli událostí, které nově budou určovat
-            náš život? Co nám nabízí současný svět? Žijeme od jedné pohromy k
-            druhé? Hospodářské, hodnotové a společenské základy se otřásají v
-            základech? Jsou souboj a válka nevyhnutelnou cestou vedoucí do
-            budoucna? Je svět vržen do náhodné a nepřehledné změti vztahů, které
-            se rozpadají a opět náhodně vznikají? Advent nám otevírá pohled na
-            obrovskou péči, s níž Stvořitel uvedl svět do života a moudře ho
-            svým milosrdenstvím vede tak, že nachází řešení i v největších
-            rébusech dějin světa. Advent nás má naučit opět doufat a s nadějí v
-            láskyplnou péči Boží žit každodenní utváření dějin osobních,
-            rodinných, národních i světových. V adventu musíme opět spatřit, jak
-            se uprostřed tohoto světa církev jako první účastní díla obrácení,
-            konverze a skutečného „zkrášlení“, tedy života z Boží milosti!
+            Umění modlitby je tak velikým darem a současně celoživotní cestou
+            růstu, vývoje a zrání. Do letošní brožury proto přispěli ti, kdo
+            přijali ve svém životě pozvání k tomuto velkému umění. Příspěvky
+            vznikaly na území naší diecéze i mimo ni, dokonce v dalekém
+            Estonsku. Tento rok organizuje naše biskupství kurz o evangelizaci,
+            jehož účastníci se také zapojili do přípravy letošní postní brožury.
+            Modlitba je základ pro předávání evangelia a sdílení živé víry s
+            druhými lidmi. Všichni jsme se nechali vést Duchem Svatým, aby nás
+            vedl v našich myšlenkách, nechal nás zakusit pohnutí ducha, dal nám
+            inspiraci k sepsání modlitby, úvahy, meditace nad textem Písma. Moc
+            rád vyjadřuji vděčnost všem autorkám i autorům letošní postní
+            brožury za to, že jako první vykročili na cestu letošního postního
+            putování. Učinili tak s jistým předstihem, texty totiž vznikaly
+            ještě před oslavou Vánoc. Řada z nich vyjádřila vnitřní obohacení,
+            které jim souvislost mezi Vánocemi a přípravou na Velikonoce
+            přinesla.
           </p>
           <p className="text-lg mb-4">
-            Jan Křtitel byl mužem, který uprostřed Zaslíbené země v čase, kdy
-            celý Izrael očekával vykoupení, vedl lid Boží ke skutečné konverzi.
-            A současně vstoupil do blízkého, bezprostředního kontaktu s tím,
-            na kterého uviděl sestupovat Ducha Svatého – na Mesiáše, Krista.
-            Advent nás má přivést opět ke Kristu. A protože on nás přišel
-            přivést „na cestu pokoje“, pozve nás, abychom z celého svého života
-            učinili „společnou cestu pokoje, lásky a milosrdenství“.
-          </p>
-          <p className="text-lg mb-4">
-            Zamyšlení nebo meditaci pro letošní adventní brožuru připravili
-            katecheté a katechetky z naší diecéze. Dlouhodobě se věnují konkrét-
-            ním dětem a mládeži, aby jim pomohli vyjít na „cestu pokoje“. K
-            vděčnosti a díkům za jejich upřímné nasazení a službu dnes navíc
-            přistupuje vděk za připravený text, reagující na evangelium, které
-            připadá na ten který adventní den.
-          </p>
-          <p className="text-lg mb-4">
-            A tak vám, milí čtenáři a čtenářky, přeji, abyste prožili letošní
-            adventní dobu s pohledem do budoucna vedeným Kristovým Duchem. Kéž
-            společně s Pannou Marií a celou církví prožijeme společnou cestu –
-            kroky, které vedou k pokoji.
+            Přeji proto vám všem, aby Světlo vtěleného Slova a Zmrtvýchvstalého
+            Krista prosvítilo celé vaše nitro, rozhlehlé prostory vašeho srdce
+            připravilo na složitost současného světa, aby „Kristus vírou
+            přebýval ve vašem srdci, abyste zakořeněni a upevněni v lásce byli
+            schopni pochopit ‒ jako všichni křesťané ‒ celou tu šířku a délku,
+            výšku i hloubku, poznat Kristovu lásku přesahující všechno poznání a
+            dosáhnout plné míry Božích (darů)“ (Ef 3,17–19).
           </p>
           <p className="text-lg mb-8 italic">Prokop Brož</p>
         </article>
--- pages/slova-biskupa-Jana.js
+++ pages/slova-biskupa-Jana.js
@@ -6,47 +6,53 @@ export default function SlovaBiskupaJana() {
   return (
     <>
       <Head>
-        <title>Slova Biskupa Jana Adventní cesta</title>
+        <title>Slova Biskupa Jana Cesta půstem</title>
       </Head>
       <Layout>
         <ArticleHeader />
         <article className="mx-6 max-w-[800px]">
           <h1 className="text-4xl font-bold mb-8">Slova biskupa Jana</h1>
           <p className="font-semibold text-lg mb-1">
-            Bratři a sestry v Kristu, milí čtenáři,
+            Bratři a sestry v Kristu,
           </p>
           <p className="text-lg mb-4">
-            všichni křesťané jsou povoláni k tomu, aby ve společenství Církve v
-            každé době a na každém kontinentu zpřítomňovali radostnou zvěst
-            Evangelia, aby ji aplikovali na konkrétní situace, oživovali,
-            nechali promlouvat k duchu dané doby. Každý z nás má jiné dary a Pán
-            ho posílá do světa s jedinečným záměrem. V tradici Církve se vyvi-
-            nulo jedno specifické povolání, které kombinuje hluboký důraz na
-            Písmo, výuku a civilní život, a to je povolání katechetů a
-            katechistů. Své znalosti denně propojují s žitou praxí, když
-            vyučují náboženství, chodí do rodin, vedou katecheze a moderují
-            setkávání pro různé skupiny věřících.
+            letošní rok má být dle přání papeže Františka velkou{" "}
+            <em>symfonií modlitby</em>, jíž se celá Církev bude připravovat na
+            jubilejní rok 2025, tedy dva tisíce let a&nbsp;čtvrt dalšího století
+            od Kristova příchodu. O&nbsp;tuto <em>symfonii</em> se pro
+            nastávající postní dobu pokusili také autoři jednotlivých příspěvků
+            v&nbsp;brožuře, kterou právě otevíráte. Všichni se ve svých textech
+            soustředí na modlitbu jakožto osobní, ale i&nbsp;společný rozhovor
+            Církve s&nbsp;Pánem, píší o&nbsp;vlastní zkušenosti a&nbsp;přinášejí
+            inspiraci nám ostatním.
           </p>
           <p className="text-lg mb-4">
-            Letošní adventní brožura je specifická tím, že prostor pro duchovní
-            zamyšlení na každý den dostávají právě oni – katechetky a katecheté
-            působící v královéhradecké diecézi. O letošním adventu se s námi
-            podělí o svou jedinečnou perspektivu, o své úvahy i praktické
-            poznatky.
+            Vždy, když se setkám s&nbsp;někým, kdo se nechá již tradiční
+            diecézní brožurou provázet po celé období den po dni, mám velikou
+            radost. Nejen proto, že to svědčí o&nbsp;individuálním úsilí
+            o&nbsp;zlepšení duchovního života, ale také proto, že se denně
+            i&nbsp;na dálku spojujeme nad stejnými texty. V průběhu postní doby
+            je udržování disciplíny duchovního života obzvláště důležitá. Rád
+            bych se přimluvil za to, abyste k&nbsp;rozjímání nad předloženými
+            texty připojili také každodenní modlitbu Růžence, třeba jen jednoho
+            desátku. Úvahy tak doplníte o&nbsp;meditativní modlitbu, která se
+            vztahuje k&nbsp;jednotlivým tajemstvím Písma, a&nbsp;zvláště pak
+            k&nbsp;Matce Boží, která nás neúnavně vede ke Kristu.{" "}
+            <em>Symfonii modlitby</em> tak zakusíte i&nbsp;ve svém individuálním
+            duchovním životě.
           </p>
           <p className="text-lg mb-4">
-            Adventní příprava na příchod našeho Pána by neměla zůstat jen u
-            teoretických úvah, ale jejím cílem je především obrácení našeho
-            srdce k Němu, zdokonalení žité víry, která se ve světě projevuje
-            konkrétními skutky lásky. Kéž nám tedy texty našich katechetů,
-            vycházející z jejich duchovního života a hluboké zkušenosti, co
-            nejúčinněji pomáhají, abychom společně přivítali Pána s otevřeným
-            srdcem.
+            Ze srdce děkuji všem, kdo se na přípravě brožury podíleli,
+            a&nbsp;jim i&nbsp;všem čtenářům přeji, aby na konci postní doby byli
+            ještě duchovně zralejší a&nbsp;pevnější než na jejím počátku.
           </p>
           <p className="text-lg mb-4">
-            K tomu Vám na prahu letošní adventní doby ze srdce žehnám!
+            K&nbsp;tomu Vám žehnám od katedrály Svatého Ducha!
           </p>
-          <p className="text-lg mb-8 italic">Váš biskup + Jan</p>
+          <p className="text-lg mb-8 italic">
+            V&nbsp;Kristy a&nbsp;Marii <br />
+            Váš biskup + Jan
+          </p>
         </article>
       </Layout>
     </>
