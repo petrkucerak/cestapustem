6b206e5106a9e8a10b21bd6ce4e3d96e25b74762
petrkucerak <petr@khome.cz>
2024-02-14 09:27:22 +0100

    Update title name

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function ArticleHeader() {
     <header className="w-full">
       <nav className="mx-6 mt-6 mb-4">
         <Link href="/" className="text-3xl font-semibold underline">
-          Adventní cesta
+          Cesta půstem
         </Link>
       </nav>
     </header>
