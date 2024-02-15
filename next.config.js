99db399861a88474a3476ae1b2d09e56c534b03d
petrkucerak <petr@khome.cz>
2024-02-15 08:43:46 +0100

    Install better plugin for caching

--- next.config.js
+++ next.config.js
@@ -1,35 +1,20 @@
-const runtimeCaching = require("next-pwa/cache");
-
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  register: true,
+  cacheOnFrontendNav: true,
+  aggressiveFrontEndNavCaching: true,
+  cacheStartUrl: true,
+  swcMinify: true,
+  publicExcludes: ["!favicon/*/**", "!audio/**"],
+  fallbacks: {
+    // Failed page requests fallback to this.
+    document: "pages/_offline.js",
+  },
+  // disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
-  // next.js config
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-  cacheOnFrontEndNav: true,
-  publicExcludes: ["!favicon/*/**", "!audio/**"],
-  buildExcludes: ["!audio/**"],
-  async headers() {
-    return [
-      {
-        source: "/den/:slug*",
-        headers: [
-          /*
-              s-maxage=2419200, // Set the max age of the cached content to a month (28 days)
-              stale-while-revalidate=1209600, // Stale whhile revalidate for a fortnight
-              stale-if-error=31536000`, // If an error occurs with the revalidation of the cached content, then the cached content will be served for a maximum of a year (365 days)
-            */
-          {
-            key: "Cache-Control",
-            value:
-              "public,s-maxage=2419200,stale-while-revalidate=1209600,stale-if-error=31536000",
-          },
-        ],
-      },
-    ];
-  },
-  runtimeCaching,
+  // Your Next.js config
+  output: "export",
+  reactStrictMode: true,
 });
