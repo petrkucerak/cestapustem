0d96a413c7c96eb59b01874573c527dc6406538d
petrkucerak <petr@khome.cz>
2026-01-31 08:29:59 +0100

    feat: init new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cestapustem.cz/",
-  integrations: [tailwind(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap()]
 });
