b542d137e139a8b61b78cc7e7d0cd88d08ad5c15
Petr Kučera <petr@khome.cz>
2026-02-09 09:53:42 +0100

    Feat/clean env (#124)
    * feat: init new project
    
    * feat: Add footer, logo, and navigation buttons components; implement layout and author pages
    
    - Created Footer.astro for consistent footer across pages.
    - Added Logo.astro component for branding.
    - Implemented NavButtons.astro for navigation between daily reflections.
    - Established RenderDate.astro for date formatting.
    - Removed Welcome.astro as it was no longer needed.
    - Configured content collections in config.ts for daily reflections.
    - Added markdown files for daily reflections and bishop messages.
    - Updated Layout.astro to include new components and improved structure.
    - Created author page to list contributors and additional resources.
    - Enhanced index.astro with project description and navigation buttons.
    - Introduced styles for primary colors in global.css.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cestapustem.cz/",
-  integrations: [tailwind(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
 });
